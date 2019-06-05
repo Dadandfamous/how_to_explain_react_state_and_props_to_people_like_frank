2d773ea7be0cc5d0044b583e5bac80dcf7a0deda
Martin <stuurmartinpost@gmail.com>
2019-06-05 19:23:48 +0200

    added button 10 years younger, hardcoded

--- src/users/Users.js
+++ src/users/Users.js
@@ -13,11 +13,21 @@ export class Users extends Component {
   }
 
 
-
+  makeMeYounger = () => {
+    this.setState({
+      users: [
+        {name:"John", age:10},
+        {name:"Jill", age:20},
+        {name:"Peter", age:30},
+      ]
+    })
+  }
 
   render() {
     return (
       <div>
+        <button onClick={this.makeMeYounger}>Make Us 10 years younger</button>
+        <br/>
         <h1>{this.state.title}</h1>
         <User age={this.state.users[0].age}>{this.state.users[0].name}</User>
         <User age={this.state.users[1].age}>{this.state.users[1].name}</User>
